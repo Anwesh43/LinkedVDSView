38abcbf56df8139ca51614c3efc9433fd5c4ebfa
Anwesh43 <anweshthecool0@gmail.com>
2018-08-18 19:23:03 +0530

    adding VerticallyDecSquareView class

--- vdsview/src/main/java/com/anwesh/uiprojects/vdsview/VerticallyDecSquareView.kt
+++ vdsview/src/main/java/com/anwesh/uiprojects/vdsview/VerticallyDecSquareView.kt
@@ -12,3 +12,20 @@ import android.graphics.Canvas
 import android.graphics.RectF
 
 val nodes : Int = 5
+
+class VerticallyDecSquareView(ctx : Context) : View(ctx) {
+
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
