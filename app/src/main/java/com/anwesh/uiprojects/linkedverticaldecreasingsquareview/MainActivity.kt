62716c9eb9640ceea6f8b507368c32beb5018b25
Anwesh43 <anweshthecool0@gmail.com>
2018-08-18 19:53:53 +0530

    using VDSView in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedverticaldecreasingsquareview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedverticaldecreasingsquareview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedverticaldecreasingsquareview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.vdsview.VerticallyDecSquareView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        VerticallyDecSquareView.create(this)
     }
 }
