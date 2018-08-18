54babb3730fd2af5e0a021640eb852aea0f5c71c
Anwesh43 <anweshthecool0@gmail.com>
2018-08-18 19:20:33 +0530

    adding vdsview module

---
+++ vdsview/src/main/java/com/anwesh/uiprojects/vdsview/VerticallyDecSquareView.kt
@@ -0,0 +1,14 @@
+package com.anwesh.uiprojects.vdsview
+
+/**
+ * Created by anweshmishra on 18/08/18.
+ */
+
+import android.view.View
+import android.content.Context
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+
+val nodes : Int = 5
