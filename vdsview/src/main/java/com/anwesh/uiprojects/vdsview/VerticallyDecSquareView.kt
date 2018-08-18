aa940c602fb96e8e746c41e8f6a8e93ba6b31921
Anwesh43 <anweshthecool0@gmail.com>
2018-08-18 19:45:54 +0530

    adding LinkedVDS class

--- vdsview/src/main/java/com/anwesh/uiprojects/vdsview/VerticallyDecSquareView.kt
+++ vdsview/src/main/java/com/anwesh/uiprojects/vdsview/VerticallyDecSquareView.kt
@@ -143,4 +143,27 @@ class VerticallyDecSquareView(ctx : Context) : View(ctx) {
         }
 
     }
+
+    class LinkedVDS(var i : Int) {
+
+        private var curr : VDSNode = VDSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
